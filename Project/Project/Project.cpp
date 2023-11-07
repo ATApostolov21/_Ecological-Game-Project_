9835e64475febdfc78a7aefdf604ea7e6279c615
IZDandarinov21 <IZDandarinov21@codingburgas.bg>
2023-11-07 21:51:22 +0200

    Add exit button

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -7,36 +7,51 @@ int main()
 
     InitWindow(screenWidth, screenHeight, "raylib menu example");
 
+    Texture2D background = LoadTexture("background.png");
+    Texture2D logo = LoadTexture("logo.png");
+
+
+
     Vector2 playButtonPosition = { (float)screenWidth / 2 - 50, (float)screenHeight / 2 - 50 };
     Vector2 exitButtonPosition = { (float)screenWidth / 2 - 50, (float)screenHeight / 2 };
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
+        DrawTexture(background, 0, 0, WHITE);
+        DrawTexture(logo, 50, 50, WHITE);
 
-
-        if (CheckCollisionPointRec(GetMousePosition(), { playButtonPosition.x, playButtonPosition.y, 100, 50 }))
-        {
+        if (CheckCollisionPointRec(GetMousePosition(), { playButtonPosition.x, playButtonPosition.y, 100, 50 })) {
             DrawText("Play", playButtonPosition.x + 18, playButtonPosition.y + 15, 20, RED);
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-            {
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
                 // Играта тук
             }
         }
 
-        else
-        {
+        else {
             DrawText("Play", playButtonPosition.x + 18, playButtonPosition.y + 15, 20, BLACK);
         }
+
+        if (CheckCollisionPointRec(GetMousePosition(), { exitButtonPosition.x, exitButtonPosition.y, 100, 50 })) {
+            DrawText("Exit", exitButtonPosition.x + 18, exitButtonPosition.y + 15, 20, RED);
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                CloseWindow();
+            }
+        }
+        else {
+            DrawText("Exit", exitButtonPosition.x + 18, exitButtonPosition.y + 15, 20, BLACK);
+        }
+
         EndDrawing();
     }
 
     CloseWindow();
 
-    
-
-
+    return 0;
 }
+
+
+
+
