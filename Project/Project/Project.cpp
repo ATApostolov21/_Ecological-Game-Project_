b88b9687caa4a3afa31d50d564dc45d3fd873d30
IZDandarinov21 <IZDandarinov21@codingburgas.bg>
2023-11-07 21:49:39 +0200

    Add play button

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -1,10 +1,42 @@
 #include "Header.h"
 
-int main() {
+int main() 
+{
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "raylib menu example");
+
+    Vector2 playButtonPosition = { (float)screenWidth / 2 - 50, (float)screenHeight / 2 - 50 };
+    Vector2 exitButtonPosition = { (float)screenWidth / 2 - 50, (float)screenHeight / 2 };
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+
+
+        if (CheckCollisionPointRec(GetMousePosition(), { playButtonPosition.x, playButtonPosition.y, 100, 50 }))
+        {
+            DrawText("Play", playButtonPosition.x + 18, playButtonPosition.y + 15, 20, RED);
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            {
+                // Играта тук
+            }
+        }
+
+        else
+        {
+            DrawText("Play", playButtonPosition.x + 18, playButtonPosition.y + 15, 20, BLACK);
+        }
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    
 
 
 }
