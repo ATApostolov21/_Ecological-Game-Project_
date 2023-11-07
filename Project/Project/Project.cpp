5b00fac6f1f9d242398696ac80a76eb964b3d86a
IZDandarinov21 <IZDandarinov21@codingburgas.bg>
2023-11-07 23:34:22 +0200

    Add background and logo

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -1,19 +1,26 @@
-#include "Header.h"
+п»ї#include "Header.h"
 
 int main() 
 {
     const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenHeight = 800;
 
     InitWindow(screenWidth, screenHeight, "raylib menu example");
 
-    Texture2D background = LoadTexture("background.png");
-    Texture2D logo = LoadTexture("logo.png");
+    Texture2D background = LoadTexture("../assets/background_menu.png");
+    Texture2D logo = LoadTexture("../assets/logo_menu.png");
 
 
 
     Vector2 playButtonPosition = { (float)screenWidth / 2 - 50, (float)screenHeight / 2 - 50 };
     Vector2 exitButtonPosition = { (float)screenWidth / 2 - 50, (float)screenHeight / 2 };
+
+    
+    Font fontTtf = LoadFontEx("../assets/PixAntiqua.ttf", 32, 0, 250);
+
+   
+
+    bool useTtf = false;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -21,27 +28,33 @@ int main()
         ClearBackground(RAYWHITE);
 
         DrawTexture(background, 0, 0, WHITE);
-        DrawTexture(logo, 50, 50, WHITE);
+        DrawTexture(logo, 100, 200, WHITE);
 
-        if (CheckCollisionPointRec(GetMousePosition(), { playButtonPosition.x, playButtonPosition.y, 100, 50 })) {
-            DrawText("Play", playButtonPosition.x + 18, playButtonPosition.y + 15, 20, RED);
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                // Играта тук
+        if (CheckCollisionPointRec(GetMousePosition(), { playButtonPosition.x, playButtonPosition.y, 100, 50 })) 
+        {
+
+            DrawText("Play", playButtonPosition.x + 18, playButtonPosition.y + 15, 40, BLUE);
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) 
+            {
+                // The game is here
             }
         }
-
-        else {
-            DrawText("Play", playButtonPosition.x + 18, playButtonPosition.y + 15, 20, BLACK);
+        else 
+        {
+            DrawText("Play", playButtonPosition.x + 18, playButtonPosition.y + 15, 40, WHITE);
         }
 
-        if (CheckCollisionPointRec(GetMousePosition(), { exitButtonPosition.x, exitButtonPosition.y, 100, 50 })) {
-            DrawText("Exit", exitButtonPosition.x + 18, exitButtonPosition.y + 15, 20, RED);
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+        if (CheckCollisionPointRec(GetMousePosition(), { exitButtonPosition.x, exitButtonPosition.y, 100, 50 })) 
+        {
+            DrawText("Exit", exitButtonPosition.x + 18, exitButtonPosition.y + 15, 40, BLUE);
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) 
+            {
                 CloseWindow();
             }
         }
-        else {
-            DrawText("Exit", exitButtonPosition.x + 18, exitButtonPosition.y + 15, 20, BLACK);
+        else 
+        {
+            DrawText("Exit", exitButtonPosition.x + 18, exitButtonPosition.y + 15, 40, WHITE);
         }
 
         EndDrawing();
