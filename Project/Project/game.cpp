12c3ef9d7cee65f2c8a2b625bc92ab98f37fe2f5
GYFilipov21 <GYFilipov21@codingburgas.bg>
2023-11-08 00:03:28 +0200

    Add test button to change menu state

--- Project/Project/game.cpp
+++ Project/Project/game.cpp
@@ -10,10 +10,14 @@ void main()
     Texture2D background = LoadTexture("../assets/background_menu.png");
     Texture2D logo = LoadTexture("../assets/logo_menu.png");
 
-
+    ////////////////
+    short state = 0;
+    ////////////////
 
     Vector2 playButtonPosition = { (float)screenWidth / 2 - 50, (float)screenHeight / 2 - 50 };
     Vector2 exitButtonPosition = { (float)screenWidth / 2 - 50, (float)screenHeight / 2 };
+    Vector2 changeMenuButton = { (float)screenWidth / 2 - 50, (float)screenHeight / 2 + 50 };
+
 
     
     Font fontTtf = LoadFontEx("../assets/PixAntiqua.ttf", 32, 0, 250);
@@ -32,19 +36,20 @@ void main()
         DrawTexture(background, 0, 0, WHITE);
         DrawTexture(logo, 100, 200, WHITE);
 
-        if (CheckCollisionPointRec(GetMousePosition(), { playButtonPosition.x, playButtonPosition.y, 100, 50 })) 
-        {
-
-            DrawText("Play", playButtonPosition.x + 18, playButtonPosition.y + 15, 40, BLUE);
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) 
+            if (CheckCollisionPointRec(GetMousePosition(), { playButtonPosition.x, playButtonPosition.y, 100, 50 }))
             {
-                // The game is here
+
+                DrawText("Play", playButtonPosition.x + 18, playButtonPosition.y + 15, 40, BLUE);
+                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                {
+                    // The game is here
+                }
             }
-        }
-        else 
-        {
-            DrawText("Play", playButtonPosition.x + 18, playButtonPosition.y + 15, 40, WHITE);
-        }
+            else
+            {
+                DrawText("Play", playButtonPosition.x + 18, playButtonPosition.y + 15, 40, WHITE);
+            }
+        
 
         if (CheckCollisionPointRec(GetMousePosition(), { exitButtonPosition.x, exitButtonPosition.y, 100, 50 })) 
         {
@@ -59,11 +64,22 @@ void main()
             DrawText("Exit", exitButtonPosition.x + 18, exitButtonPosition.y + 15, 40, WHITE);
         }
 
+        if (CheckCollisionPointRec(GetMousePosition(), { changeMenuButton.x, changeMenuButton.y, 100, 50 }))
+        {
+            DrawText("Change menu button", changeMenuButton.x + 18, changeMenuButton.y + 15, 40, BLUE);
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            {
+                CloseWindow();
+            }
+        }
+        else
+        {
+            DrawText("Change menu button", changeMenuButton.x + 18, changeMenuButton.y + 15, 40, WHITE);
+        }
         EndDrawing();
     }
 
     CloseWindow();
-
 }
 
 
